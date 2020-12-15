f1f7e72aa37d5fa8a2c5efebe35311703bd40009
m <mike@ms0.co.uk>
2020-12-15 14:38:51 +0000

    Mute toggle fade in out

--- assets/scripts.js
+++ assets/scripts.js
@@ -129,6 +129,7 @@ document.querySelector('.mute').addEventListener('click', function() {
 
   if(audio.muted) {
     audio.muted = false;
+    audio.volume = 0;
   }
 
   if(audio.paused) {
@@ -136,10 +137,45 @@ document.querySelector('.mute').addEventListener('click', function() {
   }
 
   if(this.classList.contains('mute__muted')) {
-    audio.volume = 0.4;
+    adjustVolume(audio, 0.4);
     this.classList.remove('mute__muted');
   } else {
-    audio.volume = 0;
+    adjustVolume(audio, 0);
     this.classList.add('mute__muted');
   }
 });
+
+// adjustVolume, credit - jedmao, https://stackoverflow.com/a/13149848
+async function adjustVolume(
+  element,
+  newVolume,
+  {
+      duration = 750,
+      easing = swing,
+      interval = 13,
+  } = {},
+) {
+  const originalVolume = element.volume;
+  const delta = newVolume - originalVolume;
+  if(!delta || !duration || !easing || !interval) {
+      element.volume = newVolume;
+      return Promise.resolve();
+  }
+  const ticks = Math.floor(duration / interval);
+  let tick = 1;
+  return new Promise((resolve) => {
+      const timer = setInterval(() => {
+          element.volume = originalVolume + (
+              easing(tick / ticks) * delta
+          );
+          if(++tick === ticks) {
+              clearInterval(timer);
+              resolve();
+          }
+      }, interval);
+  });
+}
+
+function swing(p) {
+  return 0.5 - Math.cos(p * Math.PI) / 2;
+}
